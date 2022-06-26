4653a786662cff4ce15bd0c9caa44a12c0052b1b
David Skotte <david.skotte@gmail.com>
2022-06-26 12:10:14 +0200

    0.0.2 - nu går det att skicka ett meddelande

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
-const { server } = require("socket.io");
+const { Server } = require("socket.io");
 
-const io = new Server 
+const io = new Server({
+  cors: {
+    origin: "*",
+    method: ["GET", "POST"],
+  },
+});
+
+// //Knex
+// const knexConfig = require("./knexfile");
+// const knex = require("knex")(knexConfig[development]);
+
+io.on("connection", (socket) => {
+  console.log(`Socket with id ${socket.id} is now connected.`);
+
+  socket.on("send-message", (data) => {
+    console.log(`${socket.id} has sent a message ${data}.`);
+    socket.broadcast.emit("send-message", data);
+  });
+});
+
+io.listen(4000);
